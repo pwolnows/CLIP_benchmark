c7a60ec110bae906f97f9772088b974eb9c4f200
Piotr Wolnowski <piotr.wolnowski@intel.com>
2025-04-18 09:04:33 +0200

    Support transformers, optimum_intel, onnx load with Python 3.10

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10'
     ],
     description="CLIP-like models benchmarks on various datasets",
     entry_points={
